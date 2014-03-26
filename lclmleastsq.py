9d4a3f08fb2460d846b32f5c4c5eeb3b3c008adc
Michael <MT@nat-oitwireless-inside-vapornet100-a-12729.princeton.edu>
2014-03-26 18:44:37 -0400

    fixed plotting bug, added smarter random LC generation and optional gaussian noise

--- lclmleastsq.py
+++ lclmleastsq.py
@@ -5,70 +5,77 @@ from lcspot import *
 import matplotlib.pyplot as plt
 import sys
 
+def genrandlc(phase=arange(0,1,.025), noisefactor=0.05):
+  while True:
+    tinc = random.uniform(0,180)
+    tlon = random.uniform(0,360)
+    tlat = random.uniform(0,90)
+    trad = random.uniform(5,10)
+    tparams = array([tinc, tlon, tlat, trad])
+    intensity = lcspot(phase, tparams)
+    if min(intensity) < .999: break
+  if noisefactor > 0.0:
+    lcrange = max(intensity) - min(intensity)
+    noise = random.normal(0,lcrange*noisefactor,len(intensity))
+    intensity = intensity + noise
+  return intensity, tparams, phase
+
 def randfit(plsplot=False):
   #generate random single-spot lightcurve
-  phase = arange(0,1,.025)
-  tinc = random.uniform(0,180)
-  tlon = random.uniform(0,360)
-  tlat = random.uniform(0,90)
-  trad = random.uniform(5,10)
-  tparams = array([tinc, tlon, tlat, trad])
-  i = lcspot(phase, tparams)
+  intensity, tparams, phase = genrandlc()
   print 'true params:', tparams
-  print 'lightcurve:', i
+  print 'lightcurve:', intensity
   
   #fit using leastsq, starting in the center of param space 
-  fitparams, iem = lcfit(phase,i)
+  fitparams, iem = lcfit(phase,intensity)
   print 'fit params:', fitparams
   print 'iem:', iem
   
   #plot
-  if plot:
+  if plsplot:
     if iem in [1,2,3,4]:
-      plt.plot(phase, i, 'r', phase, lcspot(phase,fitparams), 'b')
+      plt.plot(phase, intensity, 'r.', phase, lcspot(phase,fitparams), 'b')
       plt.xlabel('phase')
       plt.ylabel('intensity')
       plt.show()
     else:
-      plt.plot(phase, i, 'r', phase)
+      plt.plot(phase, intensity, 'r.')
       plt.xlabel('phase')
       plt.ylabel('intensity')
       plt.show()
-  return fitparams, iem
+  
+  return intensity, phase, tparams, fitparams, iem
 
 
 def randfit2(printall=False,plsplot=False):
   #generate random single-spot lightcurve
-  phase = arange(0,1,.025)
-  tinc = random.uniform(0,180)
-  tlon = random.uniform(0,360)
-  tlat = random.uniform(0,90)
-  trad = random.uniform(5,10)
-  tparams = array([tinc, tlon, tlat, trad])
-  i = lcspot(phase, tparams)
+  intensity, tparams, phase = genrandlc()
   print 'true params:', tparams
   print 'lightcurve:'
-  print i
+  print intensity
   print
   
   #fit using leastsq, starting at various points in param space
-  opts,dists,p0s = lcfits(phase,i,tparams)
+  opts,dists,p0s = lcfits(phase,intensity,tparams)
   
   #if no fits found, plot original curve and exit
   if not opts:
     print 'no fits found =('
-    plt.plot(phase, i, 'r')
+    plt.plot(phase, intensity, 'r.')
     plt.xlabel('phase')
     plt.ylabel('intensity')
     plt.show()
     return opts,dists,p0s
   
   #find best fit
-  mindist, mdi = min((val, idx) for (idx, val) in enumerate(dists))
+  bestdist, bdi = min((val, idx) for (idx, val) in enumerate(dists))
+  bestparams = opts[bdi]
+  bestp0s = p0s[bdi]
+  bestlc = lcspot(phase,bestparams)
   print 'best fit:'
-  print 'opt:',opts[mdi],'dist:',dists[mdi],'p0:',p0s[mdi]
+  print 'opt:',bestparams,'dist:',bestdist,'p0:',bestp0s
   print 'best lightcurve:'
-  print lcspot(phase,opts[mdi])
+  print bestlc
   print
   
   #print all fits
@@ -80,19 +87,19 @@ def randfit2(printall=False,plsplot=False):
   
   #plot
   if plsplot:
-    plt.plot(phase, i, 'r', phase, lcspot(phase,opts[mdi]), 'b')
+    plt.plot(phase, intensity, 'r.', phase, bestlc, 'b')
     plt.xlabel('phase')
     plt.ylabel('intensity')
     plt.show()
   
-  return opts,dists,p0s
+  return intensity, phase, tparams, opts, dists, p0s
 
 
-def lcfit(phase,i):
-  return leastsq(lcspotdiffs, [90.0,180.0,45.0,7.5], args=(phase,i))
+def lcfit(phase,intensity):
+  return leastsq(lcspotdiffs, [90.0,180.0,45.0,7.5], args=(phase,intensity))
 
 
-def lcfits(phase,i,tps):
+def lcfits(phase,intensity,tps):
   opts = []
   dists = []
   p0s = []
@@ -102,7 +109,7 @@ def lcfits(phase,i,tps):
      for rad in range(6,10,1):
       p0 = [inc,lon,lat,rad]
       try:
-        fps, iem = leastsq(lcspotdiffs, p0, args=(phase,i))
+        fps, iem = leastsq(lcspotdiffs, p0, args=(phase,intensity))
         if iem in [1,2,3,4]:
           opts.append(fps)
           dists.append(sum(((fps/tps)-1)**2))
